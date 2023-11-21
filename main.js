661f54c29bf1d590bd3d6d6c63e5e7ff88caf7f2
unknown <ruxshonaabdupattoyeva2010@gmail.com>
2023-11-21 21:19:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,128 @@
+// 30.
+function frameSoni(minut, frame) {
+    console.log(minut * frame * 60);
+    }
+
+    frameSoni(1, 1);
+    frameSoni(10, 1);
+    frameSoni(10, 25);
+    
+// 33.
+    function litrMasofa(km) {
+    console.log(km * 100 / 10);
+}
+litrMasofa(15);
+litrMasofa(23.5);
+litrMasofa(3);
+
+// 34.
+  function fun(c, d) {
+    if(c > d) {
+        console.log(c);
+    } else {
+        console.log(d);
+    }
+  }
+  fun(3, 7);
+  fun(-1, 0);
+  fun(1000, 400);
+
+// 35.
+// massiv
+    
+// 36.
+function tengStrings(str1, str2) {
+    if (str1 == str2) {
+        console.log("true");
+    } else { 
+        console.log("false");
+    }
+}
+
+tengStrings("AB", "AB");
+tengStrings("ABC", "DE");
+tengStrings("hello", "edabit");
+
+// 37.
+function boshStr(str) {
+    if(str == "") {
+        console.log("true");
+    }else {
+        console.log("false");
+    }
+    }
+    
+    boshStr("");
+    boshStr(" ");
+    boshStr("a");
+
+ // 38.
+    function bolinsin5(son) {
+        if(5 / son) {
+            console.log("true");
+        } else {
+            console.log("false");
+        }
+    }
+
+    bolinsin5(5);
+    bolinsin5(-55);
+    bolinsin5(37);
+
+// 39.
+function bolinsin100(son) {
+    if(son % 100 == 0) {
+        console.log("true");
+    } else{
+        console.log("false");
+    }
+    }
+
+    bolinsin100(1);
+    bolinsin100(1000);
+    bolinsin100(100);
+
+// 40.
+function uzunlik(str) {
+    return str.length 
+}
+console.log(uzunlik("apple"));
+// 41.
+function bolinsin(x, y) {
+    if(x % y === 0) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+    }
+    bolinsin(98, 7);
+    bolinsin(85, 4);
+
+// 42.
+function raqam(str) {
+    return +str;
+    }
+    console.log("10");
+
+    // 43.
+    function tortYuzi(k, l) {
+      console.log(k * l);
+        }
+
+        tortYuzi(3, 4);
+        tortYuzi(10, 11);
+        tortYuzi(-1, 5);
+        tortYuzi(0, 2);
+
+// 44.
+function ismFam(ism, familiya) {
+    return ism + familiya;
+}
+console.log(ismFam("Jane, Mary"));
+
+// 46.
+// massiv
+
+// 49.
+// massiv
+    
